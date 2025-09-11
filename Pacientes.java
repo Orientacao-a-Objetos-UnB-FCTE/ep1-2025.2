785cbc8aa1d0e30061cad7c3ac1e77c3f1f6e5b2
Luiz Gustavo <csouzaluizgustavo@gmail.com>
2025-09-11 19:34:41 -0300

    Classe Pacientes

---
+++ Pacientes.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pacientes implements usuariospac {
+    public String Nome;
+    public String Cpf;
+    public byte Idade;
+    public String Id;
+    public List<String> HistoricoInternacoes;
+    public List<String> HistoricoConsultas;
+
+    public Pacientes(String Nome, String Cpf, byte Idade, String Id){
+        this.Nome = Nome;
+        this.Cpf = Cpf;
+        this.Idade = Idade;
+        this.Id = Id;
+        this.HistoricoInternacoes = new ArrayList<>();
+        this.HistoricoConsultas = new ArrayList<>();
+
+    }
+    @Override
+    public String getNome() {
+        return Nome;
+    }
+    @Override
+    public byte getIdade() {
+        return Idade;
+    }
+    @Override
+    public String getId() {
+        return Id;
+    }
+    @Override
+    public String getCPF() {
+        return Cpf;
+    }
+    @Override
+    public String[] getHistoricoInternacoes() {
+        return HistoricoInternacoes.toArray(new String[0]);
+    }
+    @Override
+    public String[] getHistoricoConsultas() {
+        return HistoricoConsultas.toArray(new String[0]);
+    }
+}
