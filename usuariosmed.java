a5c60cff78fef2efea3d1d98ff80fac3a6d30e8a
Luiz Gustavo <csouzaluizgustavo@gmail.com>
2025-09-15 19:45:38 -0300

    alteração interpacientes

--- usuariosmed.java
+++ usuariosmed.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public interface usuariosmed {
+    String getnome();
+    String getcrm();
+    String getEspecialidade();
+    String getcustoDaconsulta();
+    String getagendaDeconsulta();
+    String[] gethistoricoDehorario();
 }
