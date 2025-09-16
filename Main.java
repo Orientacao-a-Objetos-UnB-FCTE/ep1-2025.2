d7cbbc7012eaaea3fad0e3dfbbca267c80213c43
Luiz Gustavo <csouzaluizgustavo@gmail.com>
2025-09-16 20:24:58 -0300

    alteração main e médicos

--- Main.java
+++ Main.java
@@ -1,16 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        Pacientes paciente0 = new Pacientes("Leon Scott Kennedy","12344233125", (byte) 27, "RE007");
-        Pacientes paciente1 = new Pacientes("Claire Redfield","32719827121", (byte) 24, "RE010");
-        Pacientes paciente2 = new Pacientes("Ellie Williams","84938173956", (byte) 14, "TLOU1");
-        Pacientes paciente3 = new Pacientes("Victor Sullivan","06183162862", (byte) 62, "UNC02");
-        Pacientes paciente4 = new Pacientes("Johnny Cage","64837162964", (byte) 34, "MK042");
-        Pacientes paciente5 = new Pacientes("John Marston","76481736121", (byte) 31, "RDR01");
-        Pacientes paciente6 = new Pacientes("Alan Wake","84917365174", (byte) 42, "AW322");
-        Pacientes paciente7 = new Pacientes("Arthur Morgan","98572764817", (byte) 36, "RDR02");
-        Pacientes paciente8 = new Pacientes("Ezio Auditore","26381749826", (byte) 29, "ASC01");
-        Pacientes paciente9 = new Pacientes("Elena Fisher","02985716234", (byte) 30, "UNC86");
-        Pacientes paciente10 = new Pacientes("Triss Merigold","44728163455", (byte) 35, "TW333");
-        Pacientes paciente11 = new Pacientes("Cranky Kong","54327714072", (byte) 81, "DK001");
+        Pacientes p0 = new Pacientes("Leon Scott Kennedy","12344233125", (byte) 27, "RE007");
+        Pacientes p1 = new Pacientes("Claire Redfield","32719827121", (byte) 24, "RE010");
+        Pacientes p2 = new Pacientes("Ellie Williams","84938173956", (byte) 14, "TLOU1");
+        Pacientes p3 = new Pacientes("Victor Sullivan","06183162862", (byte) 66, "UNC02");
+        Pacientes p4 = new Pacientes("Johnny Cage","64837162964", (byte) 34, "MK042");
+        Pacientes p5 = new Pacientes("John Marston","76481736121", (byte) 31, "RDR01");
+        Pacientes p6 = new Pacientes("Alan Wake","84917365174", (byte) 42, "AW322");
+        Pacientes p7 = new Pacientes("Arthur Morgan","98572764817", (byte) 36, "RDR02");
+        Pacientes p8 = new Pacientes("Ezio Auditore","26381749826", (byte) 29, "ASC01");
+        Pacientes p9 = new Pacientes("Elena Fisher","02985716234", (byte) 30, "UNC86");
+        Pacientes p10 = new Pacientes("Triss Merigold","44728163455", (byte) 35, "TW333");
+        Pacientes p11 = new Pacientes("Cranky Kong","54327714072", (byte) 81, "DK001");
+
+        Medicos m0 = new Medicos("Jill Valentine", "DF001", "Clínico-Geral", "R$250,00", "Segunda");
+        Medicos m1 = new Medicos("Joel Miller", "DF002", "Pediatra", "R$150,00", "Segunda");
+        Medicos m2 = new Medicos("Nathan Drake", "DF003", "Cardiologista", "R$80,00", "Segunda");
+        Medicos m3 = new Medicos("Lara Croft", "DF004", "Pneumologista", "R$125,00", "Segunda");
+        Medicos m4 = new Medicos("Geralt de Rívia", "DF005", "Infectologista", "R$110,00", "Segunda");
+        Medicos m5 = new Medicos("Gordon Freeman", "DF006", "Geriatra", "R", "Segunda");
+        Medicos m6 = new Medicos("James Sunderland", "DF007", "Psiquiatra", "R$100,00", "Segunda");
     }
+
 }
--- Medicos.java
+++ Medicos.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Medicos {
+public class Medicos implements usuariosmed {
+    private String nome;
+    private String crm;
+    private String Especialidade;
+    private String custoDaconsulta;
+    private String agendaDeconsulta;
+    private List<String>historicoDehorario;
+
+    public Medicos(String nome, String crm, String Especialidade, String custaDaconsulta, String agendaDeconsulta){
+        this.nome=nome;
+        this.crm=crm;
+        this.Especialidade=Especialidade;
+        this.custoDaconsulta=custaDaconsulta;
+        this.agendaDeconsulta=agendaDeconsulta;
+        this.historicoDehorario= new ArrayList<>();
+
+    }
+    @Override
+    public String getnome() {
+        return nome;
+    }
+    @Override
+    public String getcrm() {
+        return crm;
+    }
+    @Override
+    public String getEspecialidade() {
+        return Especialidade;
+    }
+    @Override
+    public String getcustoDaconsulta() {
+        return custoDaconsulta;
+    }
+    @Override
+    public String getagendaDeconsulta() {
+        return agendaDeconsulta;
+    }
+    @Override
+    public String[] gethistoricoDehorario() {
+        return historicoDehorario.toArray(new String[0]);
+    }
+    public void adicionarHorario(String horario){
+        historicoDehorario.add(horario);
+    }
 }
