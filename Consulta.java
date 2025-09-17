92169c3196eb2bbae078a787a5533c17780154e2
Luiz Gustavo <csouzaluizgustavo@gmail.com>
2025-09-17 19:00:02 -0300

    classe consulta com datahora

--- Consulta.java
+++ Consulta.java
@@ -2,10 +2,12 @@ public class Consulta {
     private Medicos medicos;
     private Pacientes paciente;
     private String descricao;
-    public Consulta( Medicos medicos, Pacientes paciente, String descricao){
+    private String datahorario;
+    public Consulta( Medicos medicos, Pacientes paciente, String descricao, String datahorario){
         this.paciente = paciente;
         this.medicos = medicos;
         this.descricao = descricao;
+        this.datahorario = datahorario;
     }
     public Pacientes getPacientes() {
         return paciente;
@@ -16,7 +18,10 @@ public class Consulta {
     public String getDescricao() {
         return descricao;
     }
+    public String getDatahorario() {
+        return datahorario;
+    }
     public String toString(){
-        return "Consulta com: " + medicos.getnome() + "às" +
+        return "Consulta com: " + medicos.getnome() + "Paciente-->" + paciente.getNome() + "às" + datahorario + "para" + descricao;
     }
 }
