72cdb4c285927b9fa3479e9d1746c4c24175454c
Luiz Gustavo <csouzaluizgustavo@gmail.com>
2025-09-16 21:02:57 -0300

    add pacientes e medicos no Array

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner painel = new Scanner(System.in);
+
+        List<Pacientes> pacientes = new ArrayList<>();
         Pacientes p0 = new Pacientes("Leon Scott Kennedy","12344233125", (byte) 27, "RE007");
         Pacientes p1 = new Pacientes("Claire Redfield","32719827121", (byte) 24, "RE010");
         Pacientes p2 = new Pacientes("Ellie Williams","84938173956", (byte) 14, "TLOU1");
@@ -13,6 +20,10 @@ public class Main {
         Pacientes p10 = new Pacientes("Triss Merigold","44728163455", (byte) 35, "TW333");
         Pacientes p11 = new Pacientes("Cranky Kong","54327714072", (byte) 81, "DK001");
 
+         pacientes.add(p0); pacientes.add(p1); pacientes.add(p2); pacientes.add(p3); pacientes.add(p4); pacientes.add(p5);
+         pacientes.add(p6); pacientes.add(p7); pacientes.add(p8); pacientes.add(p9); pacientes.add(p10); pacientes.add(p11);
+
+        List<Medicos> medicos = new ArrayList<>();
         Medicos m0 = new Medicos("Jill Valentine", "DF001", "Clínico-Geral", "R$250,00", "Segunda");
         Medicos m1 = new Medicos("Joel Miller", "DF002", "Pediatra", "R$150,00", "Segunda");
         Medicos m2 = new Medicos("Nathan Drake", "DF003", "Cardiologista", "R$80,00", "Segunda");
@@ -20,6 +31,10 @@ public class Main {
         Medicos m4 = new Medicos("Geralt de Rívia", "DF005", "Infectologista", "R$110,00", "Segunda");
         Medicos m5 = new Medicos("Gordon Freeman", "DF006", "Geriatra", "R", "Segunda");
         Medicos m6 = new Medicos("James Sunderland", "DF007", "Psiquiatra", "R$100,00", "Segunda");
+
+         medicos.add(m0); medicos.add(m1); medicos.add(m2); medicos.add(m3); medicos.add(m4); medicos.add(m5); medicos.add(m6);
+
+
     }
 
 }
