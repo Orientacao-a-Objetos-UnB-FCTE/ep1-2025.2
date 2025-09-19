f30599fb3a374b97580217e33c40289c3b4b907a
Luiz Gustavo <csouzaluizgustavo@gmail.com>
2025-09-19 19:09:17 -0300

    criação da internação

---
+++ Internacao.java
@@ -0,0 +1,12 @@
+public class Internacao {
+    private Pacientes pacientes;
+    private Medicos medicoResponsavel;
+    private String dataEntrada;
+    private String dataBaixa;
+    private String quarto;
+    private double custo;
+    private boolean ativa;
+
+    public Internacao(Pacientes pacientes, Medicos medicoResponsavel, String dataEntrada, String quarto, double custo){
+    }
+}
