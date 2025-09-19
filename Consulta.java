c33adf2210414ad56487bc272912e1567501237a
Luiz Gustavo <csouzaluizgustavo@gmail.com>
2025-09-18 21:41:20 -0300

    Incrementando consulta

--- Consulta.java
+++ Consulta.java
@@ -1,13 +1,19 @@
+
+
 public class Consulta {
+    public enum StatusConsulta{
+        AGENDADA, CONCLUIDA, CANCELADA}
     private Medicos medicos;
     private Pacientes paciente;
     private String descricao;
     private String datahorario;
+    private StatusConsulta status;
     public Consulta( Medicos medicos, Pacientes paciente, String descricao, String datahorario){
         this.paciente = paciente;
         this.medicos = medicos;
         this.descricao = descricao;
         this.datahorario = datahorario;
+        this.status = StatusConsulta.AGENDADA;
     }
     public Pacientes getPacientes() {
         return paciente;
@@ -21,7 +27,16 @@ public class Consulta {
     public String getDatahorario() {
         return datahorario;
     }
+    public void concluirConsulta(){
+        status = StatusConsulta.CONCLUIDA;
+    }
+    public void cancelarConsulta(){
+        status = StatusConsulta.CANCELADA;
+    }
+    public StatusConsulta getStatus() {
+        return status;
+    }
     public String toString(){
-        return "/Consulta com: " + medicos.getnome() + "Paciente-->" + paciente.getNome() + "às" + datahorario + "para" + descricao + "/";
+        return "/Consulta com: " + medicos.getnome() + "Paciente-->" + paciente.getNome() + "às" + datahorario + "para" + descricao + "/" + "que está" + status;
     }
 }
