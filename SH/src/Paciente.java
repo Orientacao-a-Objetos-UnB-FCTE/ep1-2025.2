8b6e4ec8bd081b51e39e7ed323e7485279478e1d
Lucas Nascimento Ferreira <lucasnferreiradev@gmail.com>
2025-09-22 13:11:02 -0300

    Commit Inicial

---
+++ SH/src/Paciente.java
@@ -0,0 +1,38 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Paciente {
+    private String cpf;
+    private TipoPlano plano;
+    private boolean pcd;
+    private LocalDate dataNascimento; // usar LocalDate para idade nao desatualizar
+
+
+    public Paciente(String cpf, TipoPlano plano, boolean pcd, LocalDate dataNascimento){
+        this.cpf = cpf;
+        this.plano = plano;
+        this.pcd = pcd;
+        this.dataNascimento = dataNascimento;
+    }
+    public enum TipoPlano {
+        BASICO,
+        INTERMEDIARIO,
+        ESPECIAL,
+        PREMIUM;
+    }
+    public TipoPlano getPlano(){
+        return plano;
+    }
+    public int getIdade(){
+        return Period.between(this.dataNascimento,LocalDate.now()).getYears();
+    }
+    public String getCpf(){
+        return cpf;
+    }
+    public boolean isEspecial(){
+        return (getIdade()>=60 ||this.pcd || this.plano == TipoPlano.ESPECIAL);
+    }
+    public boolean isPcd(){
+        return pcd;
+    }
+}
