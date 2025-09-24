f9b2ac6797d1d49e03df996684b44627c8ed4aca
Luiz Gustavo <csouzaluizgustavo@gmail.com>
2025-09-24 20:57:02 -0300

    Parte feita porem com alguns erros

--- GerenciarInternacao.java
+++ GerenciarInternacao.java
@@ -1,2 +1,17 @@
+import java.util.List;
+
 public class GerenciarInternacao {
+    public static boolean registrarInternacao(List<Internacao> internacoes, Pacientes paciente, Medicos medicoResponsavel, String dataEntrada, String quarto, double custo){
+        for (Internacao i : internacoes){
+            if (i.getQuarto().equalsIgnoreCase(quarto) && i.isAtiva()){
+                System.out.println("Quarto ocupado no momento!");
+                return false;
+            }
+        }
+        Internacao internacao = new Internacao(paciente, medicoResponsavel, dataEntrada, quarto, custo);
+        internacao.add(internacao);
+
+        System.out.println("Internação registrada com sucesso para: " + paciente.getNome() + "no quarto" + quarto + ".");
+        return true;
+    }
 }
