5b465e0996e18a33e549dc407732c6d1362bdf59
Luiz Gustavo <csouzaluizgustavo@gmail.com>
2025-09-25 20:53:16 -0300

    Classe quase pronta

--- GerenciarInternacao.java
+++ GerenciarInternacao.java
@@ -1,9 +1,9 @@
 import java.util.List;
 
 public class GerenciarInternacao {
-    public static boolean registrarInternacao(List<Internacao> internacoes, Pacientes paciente, Medicos medicoResponsavel, String dataEntrada, String quarto, double custo){
-        for (Internacao i : internacoes){
-            if (i.getQuarto().equalsIgnoreCase(quarto) && i.isAtiva()){
+    public static boolean registrarInternacao(List<Internacao> internacoes, Pacientes paciente, Medicos medicoResponsavel, String dataEntrada, String quarto, double custo) {
+        for (Internacao i : internacoes) {
+            if (i.getQuarto().equalsIgnoreCase(quarto) && i.isAtiva()) {
                 System.out.println("Quarto ocupado no momento!");
                 return false;
             }
@@ -13,5 +13,17 @@ public class GerenciarInternacao {
 
         System.out.println("Internação registrada com sucesso para: " + paciente.getNome() + "no quarto" + quarto + ".");
         return true;
+    }
+
+    public static boolean liberarInternacao(List<Internacao> internacoes, String quarto, String dataBaixa) {
+        for (Internacao i : internacoes) {
+            if (i.getQuarto().equalsIgnoreCase(quarto) && i.isAtiva()) {
+                i.liberarInternacao(dataBaixa);
+                System.out.println("Paciente com internação" + i.getMedicoResponsavel().getNome() + " no quarto " + quarto + " recebeu alta");
+            }
+            return true;
+        }
+        System.out.println("Nenhuma internação encontrada no qaurto" + quarto + ".");
+        return false;
     }
 }
