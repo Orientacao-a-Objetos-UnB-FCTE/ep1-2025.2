e325c3d75279ed0dc4dd65e42dffe6827cebe4f7
Luiz Gustavo <csouzaluizgustavo@gmail.com>
2025-09-26 18:19:08 -0300

    Nova classe para cadastro de exame

---
+++ Exame.java
@@ -0,0 +1,11 @@
+public class Exame {
+    public enum StatusExame{
+        AGENDADO, REALIZADO, CANCELADO
+    }
+    private Pacientes paciente;
+    private String tipo;
+    private String daraHorario:
+    private StatusExame status;
+
+    public
+}
