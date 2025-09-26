a6dd028f345df24856893b2d783b2d5b107fd980
Luiz Gustavo <csouzaluizgustavo@gmail.com>
2025-09-26 18:33:25 -0300

    Classe exame quase concluida para melhorar consulta

--- Exame.java
+++ Exame.java
@@ -4,8 +4,35 @@ public class Exame {
     }
     private Pacientes paciente;
     private String tipo;
-    private String daraHorario:
+    private String dataHorario;
     private StatusExame status;
 
-    public
+    public Exame(Pacientes paciente, String tipo, String dataHorario){
+        this.paciente = paciente;
+        this.tipo = tipo;
+        this.dataHorario = dataHorario;
+        this.status = StatusExame.AGENDADO;
+    }
+
+    public Pacientes getPaciente() {
+        return paciente;
+    }
+    public String getTipo(){
+        return tipo;
+    }
+    public String getDataHorario() {
+        return dataHorario;
+    }
+    public StatusExame getStatus(){
+        return status;
+    }
+    public void realizarExame(){
+        this.status = StatusExame.REALIZADO;
+    }
+    public void cancelarExame(){
+        this.status = StatusExame.CANCELADO;
+    }
+    public String toString(){
+        return "Exame: " + tipo + " do senhor(a): " + paciente.getNome() + " Data/Horário: " + dataHorario + " Status: " + status;
+    }
 }
